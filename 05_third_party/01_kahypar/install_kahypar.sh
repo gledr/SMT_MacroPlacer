18e73172f7276115cd5a03cee35316160684413b
Sebastian Pointner <sebastian.pointner@gmx.at>
2020-06-12 17:58:46 +0200

    Clean Up

--- 05_third_party/01_kahypar/install_kahypar.sh
+++ 05_third_party/01_kahypar/install_kahypar.sh
@@ -1,14 +1,13 @@
 #! /bin/bash 
 
-if [ ! -d jsoncpp ]; then
-    git clone https://github.com/open-source-parsers/jsoncpp 
-    cd jsoncpp
+if [ ! -d kahypar ]; then
+    git clone --depth=1 --recursive https://github.com/kahypar/kahypar
+    cd kahypar
     git checkout 1.9.2
     mkdir build
     cd build
   
     cmake -DCMAKE_INSTALL_PREFIX=../../../ ..
-
 
     make -j 2
     make install
--- 05_third_party/05_matplotlib/install_matplotlib.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash 
-
-if [ ! -d matplotlib ]; then
-    git clone https://github.com/lava/matplotlib-cpp
-    cd matplotlib-cpp
-   
-    cp matplotlibcpp.h ../../include/
-fi

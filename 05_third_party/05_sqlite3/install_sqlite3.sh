037e1be9f52302c4c00c8bc408c44d778b4e5b37
Sebastian Pointner <sebastian.pointner@gmx.at>
2020-07-07 15:51:38 +0200

    Sqlite3

--- 05_third_party/05_sqlite3/install_sqlite3.sh
+++ 05_third_party/05_sqlite3/install_sqlite3.sh
@@ -1,23 +1,17 @@
 #! /bin/bash 
 
- dir_name=sqlite-amalgamation-3300100
+lib_name=libsqlite3.a
 
-if [ ! -d $dir_name ]; then
+if [ ! -d  $lib_name ]; then
     home_dir=$(pwd)
-   
-    lib_name=libsqlite3.a
-    wget https://sqlite.org/2019/sqlite-amalgamation-3300100.zip
-    unzip sqlite-amalgamation-3300100.zip 
-   
-    cd $dir_name
-   
+    
+  
     gcc -c sqlite3.c
     ar -crs $lib_name sqlite3.o
     
    
-    cp $lib_name ../../lib
-    cp sqlite3.h ../../include
+    cp $lib_name ../lib
+    cp sqlite3.h ../include
    
     cd $home_dir
-    rm *.zip
 fi

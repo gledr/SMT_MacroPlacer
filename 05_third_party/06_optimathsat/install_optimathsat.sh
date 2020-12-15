364ed3440da01b9997f8595407a1543b41ea409b
Sebastian Pointner <sebastian.pointner@gmx.at>
2020-12-15 12:38:16 +0100

    CI

--- 05_third_party/06_optimathsat/install_optimathsat.sh
+++ 05_third_party/06_optimathsat/install_optimathsat.sh
@@ -3,10 +3,14 @@
 if [ ! -d  optimathsat-1.6.3-linux-64-bit ]; then
     wget http://optimathsat.disi.unitn.it/releases/optimathsat-1.6.3/optimathsat-1.6.3-linux-64-bit.tar.gz
     tar xf optimathsat-1.6.3-linux-64-bit.tar.gz
-  
-	if [ ! -d ../bin ]; then
-		mkdir ../bin
+
+	current_path=$(pwd)
+	cd ..
+	third_party=$(pwd)
+	
+	if [ ! -d bin ]; then
+		mkdir bin
 	fi
 
-	ln -sf $(pwd)/optimathsat-1.6.3-linux-64-bit/bin/optimathsat ../bin/
+	ln -sf  $current_path/optimathsat-1.6.3-linux-64-bit/bin/optimathsat $third_party/bin/
 fi
